9a6a6c65e35db78c1c773f01c6e3de9f7b9ca9c3
Penguinang <1157703273@qq.com>
2018-06-10 19:27:15 +0800

    简单使用了四个线程加速

---
+++ features/stochastic ray tracing/README.md
@@ -0,0 +1,6 @@
+## stochastic ray tracing
+第23组
+
++ raycasting_iteration: 1e-6
+    * 耗时50分钟
+![效果](output_test.jpg)
---
+++ features/stochastic ray tracing/render.py
@@ -0,0 +1,28 @@
+import unittest
+
+import relaxrender.points as rp
+import relaxrender.color as color
+import relaxrender.mesh as mesh
+import relaxrender.example_scene as example
+import relaxrender.raycasting as raycasting
+import relaxrender.context as ctx
+import relaxrender.screenwriter as sw
+
+class TestRelaxRender(unittest.TestCase):
+
+    def test_simple_render(self):
+        
+        scene = example.cornell_box
+        
+        myContext = ctx.Context()
+        myContext.raycasting_iteration = int(2e5)
+        render = raycasting.SimpleReverseRayCasting(myContext)
+        input_xy, output_color = render.drive_raycasting(scene)
+        
+        writer = sw.NormalizedWriter(myContext)
+        writer.write(input_xy, output_color, 'output_test.jpg')
+
+
+if __name__ == "__main__":
+    test = TestRelaxRender()
+    test.test_simple_render()
